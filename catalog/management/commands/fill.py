2d2ff593e6de83f0f13e61ebc306fb9ea7039ca8
asanov2610 <a.asanov2610@gmail.com>
2024-05-17 11:58:25 +0300

    add correction

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -12,13 +12,10 @@ class Command(BaseCommand):
         with open('catalog_categories_data.json', 'r', encoding='utf-8') as f:
             return json.load(f)
 
-
-
     @staticmethod
     def json_read_products():
         with open('catalog_products_data.json', 'r', encoding='utf-8') as f:
             return json.load(f)
-
 
     def handle(self, *args, **options):
 
@@ -34,24 +31,19 @@ class Command(BaseCommand):
                          information=category['fields']['information'])
             )
 
-
-
-
         Category.objects.bulk_create(category_for_create)
-
-
 
         for product in Command.json_read_products():
             product_for_create.append(
                 Product(
-                        id=product['pk'],
-                        name=product['fields']['name'],
-                        information=product['fields']['information'],
-                        image=product['fields']['image'],
-                        category=Category.objects.get(pk=product['fields']['category']),
-                        price=product['fields']['price'],
-                        created_at=product['fields']['created_at'],
-                        updated_at=product['fields']['updated_at'])
+                    id=product['pk'],
+                    name=product['fields']['name'],
+                    information=product['fields']['information'],
+                    image=product['fields']['image'],
+                    category=Category.objects.get(pk=product['fields']['category']),
+                    price=product['fields']['price'],
+                    created_at=product['fields']['created_at'],
+                    updated_at=product['fields']['updated_at'])
             )
 
-            Product.objects.bulk_create(product_for_create)
+        Product.objects.bulk_create(product_for_create)
